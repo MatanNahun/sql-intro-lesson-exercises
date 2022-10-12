cd78d6a4b0d10393e3fcf25c036f48a534d72eff
MatanNahum <matan.nahum95@gmail.com>
2022-10-12 14:39:05 +0300

    all exercises done

--- solutions/ex_5.sql
+++ solutions/ex_5.sql
@@ -1,7 +1,9 @@
 USE sql_intro;
 
-SELECT name, height FROM Dolphin
-WHERE healthy = 1
-ORDER BY height DESC;
+UPDATE Dolphin
+SET healthy = false
+WHERE color = "green" OR color = "brown";
+
+SELECT * FROM Dolphin
 
 
